f946098772c02a253d47b239420061fd689262f2
Sanjeeva Reddy M <m.sanjeeva.reddy.714@gmail.com>
2026-03-10 23:22:33 +0530

    Load model without compilation

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 # Load model once
-model = load_model("Blood Cell.keras")
+model = load_model("Blood Cell.keras", compile=False)
 
 class_labels = ['eosinophil', 'lymphocyte', 'monocyte', 'neutrophil']
 
